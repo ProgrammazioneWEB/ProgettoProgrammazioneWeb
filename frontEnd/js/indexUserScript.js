2b2b418d9e7d7d4e603ac20da12ff156c3aa4e8a
Lorenzo Stacchio <lorenzo.stacchio@studenti.unicam.it>
2017-09-16 11:50:45 +0200

    Creazione pagina html per modifica credenziali user

--- frontEnd/js/indexUserScript.js
+++ frontEnd/js/indexUserScript.js
@@ -438,7 +438,58 @@ indexUserApp.controller('userGraphController', function ($scope) {
 
 //modify meta controller
 indexUserApp.controller('userModifyMetaController', function ($scope, $http, $window) {
-    $scope.message="Benvenuto " + userProfile.meta.firstName +", da qui potrai modificare le tue credenziali!";
+    $scope.message = "Benvenuto " + userProfile.meta.firstName + ", da qui potrai modificare le tue credenziali!";
+    //assign data to show 
+    $scope.oldResidence = userProfile.meta.residence;
+    $scope.oldMail = userProfile.email;
+    $scope.oldPhoneNumber = userProfile.meta.numberOfPhone;
+    $scope.oldPassword = userProfile.password;
+
+    //errori rilevabili nei campi
+    $scope.mailError = { mail: "" };
+    //filter used to filter e-mails
+    var emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+
+    //function to control username field and show the errors if user wrong to type 
+    $scope.controlMailField = function () {
+        //if length is 0 user has type nothing
+        if ($scope.newMail === undefined) {
+            //username value is empty
+            $scope.mailError = { mail: "*E-mail non scritta" };
+            $scope.form.newMail.$invalid = true;
+        }
+        else {
+            if (!emailFilter.test($scope.newMail)) {
+                //username value fail the test 
+                $scope.mailError = { mail: "*Mail in formato errato" };
+                $scope.form.newMail.$invalid = true;
+            }
+            else {
+                //username is in an acceptable structure
+                $scope.mailError = { mail: "" };
+                $scope.form.newMail.$invalid = false;
+            }
+        }
+    }
+    //function that active button to modify data if at least one field is compiled
+    $scope.activeForm = function () {
+        //user have to compile at least one form field
+        if (($scope.newMail != undefined && $scope.newMail != "")
+            || ($scope.newResidence != undefined && $scope.newResidence != "" ) 
+            || ($scope.newPassword != undefined && $scope.newPassword != "" ) 
+            || ($scope.newPhoneNumber != undefined && $scope.newPhoneNumber != "" ) ) {
+            return true;
+        }
+        else return false;
+    }
+    //function to modify data, contact with server and db
+    $scope.modifyData= function(){
+        $http({
+
+        }).then(function(response){
+
+        });
+    }
 });
 
 
