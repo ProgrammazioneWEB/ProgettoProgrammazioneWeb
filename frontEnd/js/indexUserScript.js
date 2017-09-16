7fdb52111130a5ef3f754a81e413552051e9feb1
Lorenzo Stacchio <lorenzo.stacchio@studenti.unicam.it>
2017-09-16 16:28:24 +0200

    Correzione errore sintattici

--- frontEnd/js/indexUserScript.js
+++ frontEnd/js/indexUserScript.js
@@ -289,23 +289,13 @@ indexUserApp.controller('userTransactionController', function ($scope, $http, $w
 });
 
 //user spent average controller
-indexUserApp.controller('userAverageController', function ($scope) {
-    //take the date of creatione of the count
-    this.dateOfCreation = new Date(userProfile.dataCreazioneConto);
-    //take the current day
-    this.currentDay = Date.now();
-    //differences between the two dates
-    this.numberOfDays = this.currentDay - this.dateOfCreation.getDate();
-    //take the sum of the passive
-    this.sumOfPassive = 0;
-    //define a function into the for each called on the movement's array to find the average of the spent
-    //of the user for every day
-    /** 
-    userProfile.movimenti.forEach(function (movimento) {
-        this.sumOfPassive += Number(movimento.spesa);
-    }, this);*/
-    //return the sum divided by the total of the days with some activity
-    this.average = Math.round(this.sumOfPassive / this.numberOfDays).toFixed(2);
+indexUserApp.controller('userAverageController', function ($scope,$http) {
+   //take the average of spent and entrance
+    $http({
+
+   }).then(function(response){
+
+   });
     $scope.message = "La tua spesa giornaliera media è di: " + this.average;
 });
 
@@ -508,11 +498,11 @@ indexUserApp.controller('userModifyMetaController', function ($scope, $http, $wi
                 'password': $scope.newPassword,
                 'phone': $scope.newPhoneNumber,
                 'residence': $scope.newResidence
-
             }
         }).then(function (response) {
             if(response.data.success){
                 alert(response.data);
+                $window.location.reload();
             }
             else{
                 alert(response.data.message);
