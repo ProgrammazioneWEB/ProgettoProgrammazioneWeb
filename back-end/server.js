ba8b1a6c48d50987b2566cb03354ed4bdee55d73
LupiniMatteo <matteo.lupini@studenti.unicam.it>
2017-09-28 15:25:10 +0200

    cancellazione aggiunte mongooose

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,7 @@ var express = require('express'); // Used for the route
 var app = express();
 var bodyParser = require('body-parser'); // Parsing middleware
 var morgan = require('morgan'); // Logger middleware
-var mongoose = require('mongoose'); // MongoDB
+
 
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
@@ -26,7 +26,7 @@ var DataStoreController = require('./dataStore');
 // =======================
 var port = 3001 || process.env.PORT; // used to create, sign, and verify tokens
 var ip = "localhost";
-mongoose.connect(config.database); // connect to database
+
 app.set('superSecret', config.secret); // secret variable (prelevata da config.js)
 
 // use body parser so we can get info from POST and/or URL parameters
